e74c6b6a1092f59c2bc1abd0b540826557d5b628
Myagkov Dmitry <dima.myagkov1@yandex.ru>
2024-08-28 15:31:52 +0300

    Добавил принтов но получше и вроде как передал параметр в запуск uvicorn

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sys
-
 import requests
 import os
+
+import uvicorn
 import yaml
 import logging
 import socket
@@ -32,7 +33,7 @@ class TrivyHealthExporter:
         except requests.RequestException:
             self.health_status = 0
 
-    def collect(self):  
+    def collect(self):
         self.check_health()  # Выполняем проверку перед сбором метрик
 
         health_metric = GaugeMetricFamily(
@@ -70,7 +71,6 @@ elif config.get("trivy", {}).get("url"):
     trivy_url = config.get("trivy", {}).get("url")
     url_source = "configuration file config.yml"
 
-
 logger.info(f"Using Trivy URL: {trivy_url} (source: {url_source})")
 print(f"Using Trivy URL: {trivy_url} (source: {url_source})")
 # Получение IP-адреса сервера
@@ -78,15 +78,14 @@ server_ip = socket.gethostbyname(socket.gethostname())
 
 # Порт, на котором будет запущено приложение
 server_port = int(os.getenv("UVICORN_PORT", 8000))  # Можно также указать порт в переменной окружения
-print(os.getenv("TRIVY_SERVER_URL"))
-print(os.getenv("UVICORN_PORT"))
+print("Printing TRIVY_SERVER_URL from OS" + "\n" + str(os.getenv("TRIVY_SERVER_URL")))
+print("Printing UVICORN_PORT from OS" + "\n" + str(os.getenv("UVICORN_PORT")))
 
 # Извлечение порта из аргументов командной строки
 if '--port' in sys.argv:
     port_index = sys.argv.index('--port') + 1
     if port_index < len(sys.argv):
         server_port = int(sys.argv[port_index])
-
 
 logger.info(f"Server IP Address: {server_ip}")
 logger.info(f"Server Port: {server_port}")
@@ -110,3 +109,5 @@ def metrics():
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=server_port)
