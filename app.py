822be5050065bbccce9e5901d5e0f0c76291fd54
Myagkov Dmitry <dima.myagkov1@yandex.ru>
2024-08-29 12:55:25 +0300

    Поменял под Appsec.Track

--- app.py
+++ app.py
@@ -17,32 +17,50 @@ app = FastAPI(debug=False)
 logger = logging.getLogger(__name__)  # Используем логгер Uvicorn с уровнем INFO
 
 
-class TrivyHealthExporter:
-    def __init__(self, trivy_url):
-        self.trivy_url = trivy_url
+class TrackHealthExporter:
+    def __init__(self, track_url):
+        self.track_url = track_url
         self.health_status = 0  # 0 — не работает, 1 — работает
-        logger.info(f"TrivyHealthExporter instance created with URL: {self.trivy_url}")
+        logger.info(f"TrackHealthExporter instance created with URL: {self.track_url}")
 
     def check_health(self):
         try:
-            response = requests.get(f"{self.trivy_url}/healthz", timeout=2, verify=False)
-            if response.status_code == 200 and response.text.strip().lower() == "ok":
+            # Sending the request to the specified URL
+            response = requests.get(f"{self.track_url}/api/info",
+                                    params={"purl": "pkg%3Amaven%2Forg.dom4j%2Fdom4j%402.1.1"},
+                                    timeout=5, verify=False)
+            response.raise_for_status()  # Raises an error for bad responses (4xx/5xx)
+
+            # Parsing the JSON response
+            data = response.json()
+
+            # Extracting necessary information from the response
+            versions = data.get("versions", [])
+            vulnerabilities_count = sum(len(version.get("vulnerabilityIds", [])) for version in versions)
+
+            if vulnerabilities_count > 0:
+                # If there are vulnerabilities, set health_status to 0
                 self.health_status = 1
+                logger.info(f"Vulnerabilities found: {vulnerabilities_count}")
             else:
-                self.health_status = 0
-        except requests.RequestException:
+                # If no vulnerabilities are found, set health_status to 1
+                self.health_status = 1
+                logger.info("No vulnerabilities found.")
+
+        except requests.RequestException as e:
+            logger.error(f"Request failed: {e}")
             self.health_status = 0
 
     def collect(self):
         self.check_health()  # Выполняем проверку перед сбором метрик
 
         health_metric = GaugeMetricFamily(
-            "trivy_health_status",
-            "Состояние здоровья сервера Trivy (1 — работает, 0 — не работает)",
+            "track_health_status",
+            "Состояние здоровья сервера Track (1 — работает, 0 — не работает)",
             labels=["service"]
         )
 
-        health_metric.add_metric(["trivy"], self.health_status)
+        health_metric.add_metric(["track"], self.health_status)
         yield health_metric
 
 
@@ -61,24 +79,24 @@ config = load_config(config_path)
 
 # Проверка на наличие ключа "url" в конфиге
 default_url = "http://localhost:4954"
-trivy_url = default_url
+track_url = default_url
 url_source = "default_url"
 
-if "TRIVY_SERVER_URL" in os.environ:
-    trivy_url = os.getenv("TRIVY_SERVER_URL")
-    url_source = "environment variable TRIVY_SERVER_URL"
-elif config.get("trivy", {}).get("url"):
-    trivy_url = config.get("trivy", {}).get("url")
+if "TRACK_SERVER_URL" in os.environ:
+    track_url = os.getenv("TRACK_SERVER_URL")
+    url_source = "environment variable TRACK_SERVER_URL"
+elif config.get("track", {}).get("url"):
+    track_url = config.get("track", {}).get("url")
     url_source = "configuration file config.yml"
 
-logger.info(f"Using Trivy URL: {trivy_url} (source: {url_source})")
-print(f"Using Trivy URL: {trivy_url} (source: {url_source})")
+logger.info(f"Using track URL: {track_url} (source: {url_source})")
+print(f"Using track URL: {track_url} (source: {url_source})")
 # Получение IP-адреса сервера
 server_ip = socket.gethostbyname(socket.gethostname())
 
 # Порт, на котором будет запущено приложение
 server_port = int(os.getenv("UVICORN_PORT", 8000))  # Можно также указать порт в переменной окружения
-print("Printing TRIVY_SERVER_URL from OS" + "\n" + str(os.getenv("TRIVY_SERVER_URL")))
+print("Printing TRACK_SERVER_URL from OS" + "\n" + str(os.getenv("TRACK_SERVER_URL")))
 print("Printing UVICORN_PORT from OS" + "\n" + str(os.getenv("UVICORN_PORT")))
 
 # Извлечение порта из аргументов командной строки
@@ -98,14 +116,14 @@ async def read_root():
 
 
 # Создаем экспортера с использованием конфигурации
-trivy_health_exporter = TrivyHealthExporter(trivy_url)
-REGISTRY.register(trivy_health_exporter)
+track_health_exporter = TrackHealthExporter(track_url)
+REGISTRY.register(track_health_exporter)
 
 
 @app.get("/metrics")
 def metrics():
-    trivy_health_exporter.check_health()  # Обновляем статус перед возвращением метрик
-    print(f"TrivyHealthExporter.health_status=", trivy_health_exporter.health_status)
+    track_health_exporter.check_health()  # Обновляем статус перед возвращением метрик
+    print(f"trackHealthExporter.health_status=", track_health_exporter.health_status)
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 
