b0a9b6307c75aa0ba4c7d51b4089cfdc16f0a53d
Myagkov Dmitry <dima.myagkov1@yandex.ru>
2024-10-21 23:29:34 +0300

    Рефакторинг

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import sys
+from pprint import pprint
+
 import requests
 import os
 
@@ -21,31 +23,34 @@ class TrackHealthExporter:
     def __init__(self, track_url):
         self.track_url = track_url
         self.health_status = 0  # 0 — не работает, 1 — работает
+        self.disk_total = 0.0
+        self.disk_free = 0.0
         logger.info(f"TrackHealthExporter instance created with URL: {self.track_url}")
 
     def check_health(self):
         try:
             # Sending the request to the specified URL
-            response = requests.get(f"{self.track_url}/api/info",
-                                    params={"purl": "pkg%3Amaven%2Forg.dom4j%2Fdom4j%402.1.1"},
-                                    timeout=5, verify=False)
+            response = requests.get(f"{self.track_url}/actuator/health/custom",
+                                    timeout=2, verify=False)
             response.raise_for_status()  # Raises an error for bad responses (4xx/5xx)
 
             # Parsing the JSON response
-            data = response.json()
+            health_data = response.json()
 
-            # Extracting necessary information from the response
-            versions = data.get("versions", [])
-            vulnerabilities_count = sum(len(version.get("vulnerabilityIds", [])) for version in versions)
+            if not health_data:
+                logger.warning("JSON is empty")
+                return
+            pprint(health_data)
 
-            if vulnerabilities_count > 0:
-                # If there are vulnerabilities, set health_status to 0
+            if health_data.get("status") == "UP":
                 self.health_status = 1
-                logger.info(f"Vulnerabilities found: {vulnerabilities_count}")
             else:
                 # If no vulnerabilities are found, set health_status to 1
-                self.health_status = 1
-                logger.info("No vulnerabilities found.")
+                self.health_status = 0
+            if "components" in health_data and "diskSpace" in health_data["components"]:
+                if health_data["components"]["diskSpace"]["status"] == "UP":
+                    self.disk_total = round(health_data["components"]["diskSpace"]["details"]["total"] / (1024 ** 3), 2)
+                    self.disk_free = round(health_data["components"]["diskSpace"]["details"]["free"] / (1024 ** 3), 2)
 
         except requests.RequestException as e:
             logger.error(f"Request failed: {e}")
@@ -56,11 +61,13 @@ class TrackHealthExporter:
 
         health_metric = GaugeMetricFamily(
             "track_health_status",
-            "Состояние здоровья сервера Track (1 — работает, 0 — не работает)",
+            "Состояние здоровья сервера Track (1 — работает, 0 — не работает), общее дисковое пространство, свободное",
             labels=["service"]
         )
 
-        health_metric.add_metric(["track"], self.health_status)
+        health_metric.add_metric(["UP"], self.health_status)
+        health_metric.add_metric(["disk_total_GB"], self.disk_total)
+        health_metric.add_metric(["disk_free_GB"], self.disk_free)
         yield health_metric
 
 
